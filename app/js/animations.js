13711890fd45738b0952f5b20b74f178fcee198a
bobby <xaevir@gmail.com>
2014-07-10 12:15:38 -0400

    removing description field as dynamic

--- app/js/animations.js
+++ app/js/animations.js
@@ -1,111 +1,176 @@
 'use strict';
 
 angular.module('wrapApp')
-  .animation('.test-replace', function($window) {
+
+  .animation('.drop-out', function($window) {
+    var duration = 1;
     return {
       enter: function(element, done) {
 
+        TweenMax.set(element, {opacity: 0});
 
+        TweenMax.to(element, 0, {y:70 });
+
+        TweenMax.to(element, 1, {
+          y:0,
+          opacity: 1,
+          delay:0.1,
+          ease:'Elastic.easeOut'
+        });
+
+        $window.setTimeout(done, duration * 1000);
       },
       leave: function(element, done) {
 
+        TweenMax.to(element, 1, {
+          y:70,
+          opacity: 0,
+          //ease:'Elastic.easeIn'
+        });
+
+        $window.setTimeout(done, duration * 1000);
       },
     };
   })
 
-
-
-  .animation('.flip-replace', function($window) {
+  .animation('.multi-bounce', function($window) {
+    var duration = 1;
     return {
       enter: function(element, done) {
 
-      var $slogans = $("p.slogan").find("strong");
-          var $holder = $("#holder");
+        //var t1 = new TimelineLite();
 
-          //set via JS so they're visible if JS disabled
-          $slogans.parent().css({position : "absolute", top:"0px", left:"0px"});
-
-          //settings
-          var transitionTime = 0.4;
-          var slogansDelayTime = 2;
-
-          //internal
-          var totalSlogans = $slogans.length;
-
-          var oldSlogan = 0;
-          var currentSlogan = -1;
-
-          //initialize	
-          switchSlogan();
-
-          function switchSlogan(){
-
-            oldSlogan = currentSlogan;
-
-            if(currentSlogan < totalSlogans-1){
-              currentSlogan ++
-            } else {
-              currentSlogan = 0;
-            }
-
-            TweenLite.to($slogans.eq(oldSlogan), transitionTime, {top:-20, alpha:0, rotationX: 90});
-            TweenLite.fromTo($slogans.eq(currentSlogan), transitionTime, {top:20, alpha:0, rotationX: -90 }, {top:0, alpha:1, rotationX:0});
-
-            TweenLite.delayedCall(slogansDelayTime, switchSlogan);
-          }
-
-
-
-
-
-
-
-
-        var wrapper = $('<div></div>');
-        wrapper.css({
-          position : 'absolute',
-          top: '0px',
-          left: '0px',
-          perspective: 80
+        TweenMax.from(element, 0.5, {
+          width:0,
+          ease: 'Power4.easeInOut'
         });
 
-        element.wrap(wrapper);
+        //element.css({opacity: 0});
+        //t1.fromTo(element, 1, {width: 0}, {width: element.width()})
+          //.from(element, 1, {x: 10})
+          //.from(element, 1, {x: 30, y: 70});
+
+        $window.setTimeout(done, duration * 1000);
+      },
+      leave: function(element, done) {
+        TweenMax.to(element, 0.5, {
+          width:0,
+          ease: 'Power0.easeIn'
+        });
+
+        $window.setTimeout(done, 0.5 * 1000);
+      },
+    };
+  })
+
+  .animation('.bounce-fade', function($window) {
+
+    var duration = 1;
+
+    return {
+      enter: function(element, done) {
+
+        element.parent().height(element.height());
+
+        TweenMax.set(element, {
+          position: 'absolute',
+        });
+
+        TweenMax.to(element, 0.3, {
+          y: -20
+        });
+
+        TweenMax.to(element, 0.5, {
+          y: 0,
+          delay: 0.3,
+          ease: 'Elastic.easeOut'
+        });
+
+        $window.setTimeout(done, duration * 1000);
+      },
+      leave: function(element, done) {
+
+        TweenMax.to(element, .3, {
+          height: 0,
+          opacity: 0,
+          y: -30,
+        });
+        $window.setTimeout(done, duration * 1000);
+      }
+    };
+  })
+
+
+  .animation('.bounce', function($window) {
+    return {
+      enter: function(element, done) {
+        var duration = 1;
+
+        TweenMax.to(element, 0.3, {
+          y: -20
+        });
+
+        TweenMax.to(element, 0.5, {
+          y: 0,
+          delay: 0.3,
+          ease: 'Elastic.easeOut'
+        });
+
+        $window.setTimeout(done, duration * 1000);
+      }
+    };
+  })
+  .animation('.flip-replace', function($window) {
+
+    var topValue = '';
+
+    return {
+      enter: function(element, done) {
+
+       var elementHeight = element.height();
+
+       topValue = elementHeight/2;
 
         TweenMax.set(element.parent(), {
-          height: element.height()
+          height: elementHeight
         });
 
         TweenMax.set(element, {
+          position: 'absolute',
           textRendering: 'optimizeLegibility'
         });
 
-        var duration = 0.4;
+        var duration = 0.7;
 
         TweenMax.fromTo(element, duration, {
-          top:20,
-          opacity:0,
-          rotationX: -90
+          top: '-'+topValue,
+          opacity: 0,
+          rotationX: 90
         }, {
           top:0,
-          alpha:1,
+          opacity:1,
           rotationX:0
         });
 
         $window.setTimeout(done, duration * 1000);
       },
       leave: function(element, done) {
-        var duration = 0.4;
+        var duration = 0.7;
         TweenMax.to(element, duration, {
-          top:-20,
+          top: topValue,
           opacity:0,
-          rotationX: 90,
+          rotationX: -90,
         });
 
         $window.setTimeout(done, duration * 1000);
       },
     };
   })
-  .animation('.fade-replace', function($window) {
+
+  .animation('.fade-replace-1', function($window) {
+
+    var duration = 1;
+
     return {
       enter: function(element, done) {
 
@@ -116,19 +181,50 @@ angular.module('wrapApp')
           opacity: 0
         });
 
-        TweenMax.to(element, 1, {
+        TweenMax.to(element, 0.3, {
           opacity:1,
           display: 'block',
-          delay: 1
+          delay: 0.4,
+          onComplete: done
         });
 
-        var duration = 1;
-        $window.setTimeout(done, duration * 1000);
       },
       leave: function(element, done) {
-        var duration = 1;
-        TweenMax.to(element, 1, {opacity:0});
-        $window.setTimeout(done, duration * 1000);
+
+        TweenMax.to(element, 0.3, {
+          opacity:0,
+          delay: 0.1,
+          onComplete: done
+        });
       },
+    };
+  })
+
+  .animation('.fade-replace', function($window) {
+
+    var duration = 1;
+
+    return {
+      enter: function(element, done) {
+
+        element.parent().height(element.height());
+
+        element.css({
+          display:'none',
+          opacity: 0
+        });
+
+        TweenMax.to(element, .3, {
+          opacity:1,
+          display: 'block',
+          delay: 0.3,
+          onComplete: done
+        });
+
+      },
+      leave: function(element, done) {
+
+        TweenMax.to(element, 0.3, {opacity:0, onComplete: done });
+      }
     };
   });
